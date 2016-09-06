00ca4baf0a4060ac5699d3851039b23651e15f88
Bryan Gronberg <btgr91@gmail.com>
2016-09-05 20:59:21 -0600

    Add files via upload

---
+++ file.js
@@ -0,0 +1,10 @@
+var total=Math.random();
+
+if (total > .5) {
+  document.write("<h1>Go LEFT!!!!</h1>" + '<img src="img/left.png">');
+
+}
+
+else{
+  document.write("<h1>Go RIGHT!!!!</h1>" + '<img src="img/right.png">');
+}
